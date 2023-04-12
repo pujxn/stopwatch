5703631c749cdf3708b89f6b9212192f89807033
pujan <pujxnp@gmail.com>
2023-04-13 00:06:56 +0530

    Fixed stop and reset functionality. value now doesnt rest to 0 on stop, thanks to state var

--- src/components/DisplayLogic.jsx
+++ src/components/DisplayLogic.jsx
@@ -1,34 +1,39 @@
 import useTimer from "easytimer-react-hook";
 import Display from "@/components/Display"
 import Controls from "@/components/Controls";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const DisplayLogic = () => {
 
+    const [time, setTime] = useState("00:00:00");
     const [timer] = useTimer();
-
     const [playState, setPlayState] = useState(false);
+
+
 
     const handleToggle = () => {
         console.log(playState)
         setPlayState((prevState) => !prevState);
-        playState && timer.pause();
+        playState && (
+            setTime(timer.getTimeValues().toString()),
+            timer.pause()
+        );
         !playState && timer.start();
     }
 
     const handleStop = () => {
+        setTime(timer.getTimeValues().toString());
         setPlayState(false);
         timer.stop();
     }
 
-    // const handleReset = () => {
-    //     timer.stop();
-    // }
-
-    // timer.start();
+    const handleReset = () => {
+        setPlayState(true);
+        timer.reset();
+    }
     return (
         <>
-            <Display time={timer.getTimeValues().toString()} />
-            <Controls toggle={handleToggle} playState={playState} stop={handleStop} reset={() => timer.reset()} />
+            <Display time={playState ? timer.getTimeValues().toString() : time} />
+            <Controls toggle={handleToggle} playState={playState} stop={handleStop} reset={handleReset} />
         </>
 
     )
